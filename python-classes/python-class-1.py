0b53988eafe3249c9f8913e3392b1c42bf9fa7cf
Muhammad Hamza <36566967+Hamzakh3@users.noreply.github.com>
2023-07-08 12:58:34 +0500

    First Class Code

--- python-classes/python-class-1.py
+++ python-classes/python-class-1.py
@@ -26,9 +26,25 @@ print("A", "B", "C", sep="|")
 print(2 + 2)
 # Note: Dont do this with numbers i.e. print("2 + 2")
 
+
 # Lesson 5 | Single Comment and Multiline Comment
 # Single Line Comment (#)
 # Multiline Comment ("""Your text""")
+
+# Lesson 6 | Variables
+name = "Hamza" #string
+age = 26 #integer
+height = 6.0 #float
+print(name, age, height)
+
+# Lesson 7 | Variable name limits
+# 30_cars = 30 invalid
+# 30*cars = 30 invalid
+# 30-cars = 30 invalid
+
+# myCars = 30 camel case
+# my_cars = 30 snake case
+# MyCars = 30 Pascal
 
 # Exercises
 # Display 2 subject name and their score i.e. Math:50
@@ -40,3 +56,15 @@ print("Math:", 50 )
 
 # Display the word: There are three "chickens" in the house
 print('There are three "chickens" in the house')
+
+name = "Hamza" #string
+iAm = "JavaScript Developer"
+live = "Karachi"
+
+print("My name is", name)
+print("I, am", iAm)
+print("I live in", live)
+
+print("My name is", name, end=", ")
+print("I am", iAm, end=". ")
+print("I live in", live, end="")
