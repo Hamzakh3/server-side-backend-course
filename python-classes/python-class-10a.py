26fd8054ae499b2e2daeee9fefaa09221f7c8e71
Muhammad Hamza <36566967+Hamzakh3@users.noreply.github.com>
2023-09-16 13:14:14 +0500

    topics

---
+++ python-classes/python-class-10a.py
@@ -0,0 +1,15 @@
+# help(class name)
+# MRO : Method resolution order
+# Inheritance class Car(Animals)
+# Polymorphism -over loading - over riding
+# Method over riding, over ride parent method
+# Abstract Method
+class Animals:
+    def __init__(
+            self, 
+            name, 
+            breed, 
+            weight
+            ):
+
+        pass
